b4ba49f3e35d63d17a7ca099f969bfcec275c5f2
yasinevren <yasinevren222@gmail.com>
2017-02-25 19:53:32 +0300

    Yeni Değişiklikler

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <?php
-$isim= 4.35;
-$tirrek = intval($isim);
-echo $tirrek;
+
+class film{
+	public id;
+	public adi;
+	public yili;
+	public turu;
+	public mensei;	
+}
+#---------------------------------------------
+$esaretinbedeli = new film();
+$esaretinbedeli -> id=1;
+$esaretinbedeli -> adi="Esaretin Bedeli";
+$esaretinbedeli -> yili="1995";
+$esaretinbedeli -> turu="Dram";
+$esaretinbedeli -> mensei="Amerika";
+#---------------------------------------------
+$gora = new film();
+$gora -> id=2;
+$gora -> adi="G.O.R.A";
+$gora -> yili="2004";
+$gora -> turu="Komedi";
+$gora -> mensei="Türkiye";
+#---------------------------------------------
+$matrix = new film();
+$matrix -> id=3;
+$matrix -> adi="Matrix";
+$matrix -> yili="1999";
+$matrix -> turu="Aksiyon";
+$matrix -> mensei="Amerika";
+#---------------------------------------------
+$amelie = new film();
+$amelie -> id =4;
+$amelie -> adi ="Amélie";
+$amelie -> yili ="2001";
+$amelie -> turu ="Romantik Komedi";
+$amelie -> mensei ="Fransa";
+#---------------------------------------------
+$suburra = new film();
+$suburra -> id =5;
+$suburra -> adi ="Suburra";
+$suburra -> yili ="2015";
+$suburra -> turu ="Suç ve Polisiye";
+$suburra -> mensei ="Ýtalyan";
+#---------------------------------------------
+
+
+
+
 ?>
